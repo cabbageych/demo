e5cd372ffe3164a98ac49b8fa1caee6deda16222
cabbageych <1109685259@qq.com>
2019-05-22 16:42:20 +0800

    upload

---
+++ app/js/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Router from 'vue-router' //导入路由模块
+import VueRouter from 'vue-router'
+
+
+//使用路由
+Vue.use(Router)
+//新建路由
+
+
+
+
+export default new Router({
+    routes: [
+        {
+            path:'/',
+            name:"myBody",
+            component:myBody
+        },
+    ]
+})

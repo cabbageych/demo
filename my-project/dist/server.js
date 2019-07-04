d98fc905cc413f786c7ea6b6ffc6204fc81fe462
cabbageych <1109685259@qq.com>
2019-07-04 16:27:01 +0800

    update

---
+++ my-project/dist/server.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+
+var server = http.createServer((request,response)=>{
+    let pathname = url.parse(request.url).pathname;
+    pathname = pathname.substr(1);
+    fs.readFile(pathname,(err,data)=>{
+        if(err){
+            response.writeHead(404,"Content-Type:text/html");
+        }else{
+            response.writeHead(200,"Content-Type:text/html");
+            response.write(data.toString());
+        }
+        response.end();
+    });
+});
+
+server.listen(8080);
